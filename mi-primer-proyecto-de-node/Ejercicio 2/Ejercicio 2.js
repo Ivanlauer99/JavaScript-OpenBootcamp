df70ad7c48daed5d7b64e6cfb97a2b3793bde66a
Ivanlauer99 <ivanlauer99@gmail.com>
2022-12-12 23:12:36 -0300

    Primer commit

---
+++ mi-primer-proyecto-de-node/Ejercicio 2/Ejercicio 2.js
@@ -0,0 +1,17 @@
+let nombre = "Ivan";
+console.log(nombre);
+
+let edad = 22;
+console.log(edad);
+
+let desarrollador = false;
+console.log(desarrollador);
+
+let nacimiento = new Date (2000, 9, 24);
+console.log(nacimiento);
+
+const libro = {
+    titulo : "Luna de pluton",
+    autor : "Dross",
+    fecha : new Date(2012, 8, 20),
+}
